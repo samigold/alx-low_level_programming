385dae36033c57750f12453093b5eb4806287874
samigold <51761415+samigold@users.noreply.github.com>
2022-09-11 20:31:10 -0400

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<unistd.h>
 /**
  * main - Entry point
  * Return - 1 (Success)
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
