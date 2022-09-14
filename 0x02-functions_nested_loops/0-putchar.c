66c185ba4af7f9d5d545383fb1772368a282733c
samigold <samigold8650@gmail.com>
2022-09-15 00:47:51 +0100

    betty coding style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,10 @@
  * main - Entry point
  * Return: 0 (success)
  */
-int main(void){
-	char str[]="_putchar";
-	printf("%s\n",str);
-	return 0;
+int main(void)
+{
+	char str[] = "_putchar";
+
+	printf("%s\n", str);
+	return (0);
 }
