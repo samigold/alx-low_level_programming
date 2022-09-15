8cd523b25a7e5942368416daea0199c33c7ac1a3
samigold <samigold8650@gmail.com>
2022-09-15 04:51:54 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@ int print_sign(int n)
 {
 	if(n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
-	} elseif(n == 0)
+	} else if(n == 0)
 	{
-		printf("0")l;
+		_putchar('0');
 		return (0);
 	} else 
 	{
-		printf("-");
+		_putchar('-');
 		return (-1);
 	}
 }
