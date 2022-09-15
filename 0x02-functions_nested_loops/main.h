0809168aaf7d0a48ffe7a2d408c143e35ebc8e36
samigold <samigold8650@gmail.com>
2022-09-15 05:38:44 +0100

    low level programming

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
-int _putchar (char);
+int _putchar(char c);
 
 void print_alphabet(void);
 
@@ -13,15 +13,17 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
-int _abs(int);
+int _abs(int n);
 
-int print_last_digit(int);
+int print_last_digit(int n);
 
 void jack_bauer(void);
 
 void times_table(void);
 
-int add(int, int);
+int add(int a, int b);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
 #endif
