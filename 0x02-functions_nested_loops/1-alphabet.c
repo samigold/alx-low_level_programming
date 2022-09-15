54530aa21cac8cf9391f4e9599840285920b9907
samigold <samigold8650@gmail.com>
2022-09-15 01:30:21 +0100

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * print_alphabet - Entry point
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	char lower_case = 'a';
+
+	while(lower_case <= 'z')
+	{
+		putchar(lower_case);
+		lower_case++;
+	}
+	putchar('\n');
+	return (0);
+}
