ada811c7a2d1a0f2b4d6291a6b825f8f19f577de
samigold <samigold8650@gmail.com>
2022-09-15 04:44:04 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to checked
+ * Return: 1, 0, -1
+ */
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		printf("+");
+		return (1);
+	} elseif(n == 0)
+	{
+		printf("0")l;
+		return (0);
+	} else 
+	{
+		printf("-");
+		return (-1);
+	}
+}
