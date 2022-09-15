b67ef0f63729671d7d049edb22b338b278f05442
samigold <samigold8650@gmail.com>
2022-09-15 05:14:33 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * _abs - finds the absolute value of an integer
+ * @ab: integer input
+ * Return: 0 (success)
+ */
+int _abs(int)
+{
+	return (ab *((ab > 0) - (ab < 0)));
+}
