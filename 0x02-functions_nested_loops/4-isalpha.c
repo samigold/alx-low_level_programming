4cd7f3713911e769e42c063ad1333900db12575f
samigold <samigold8650@gmail.com>
2022-09-15 04:08:35 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks for upper case
+ * int c: the characters to be checked
+ * Return: 1 (success), 0 (failure)
+ */
+int _isalpha(int c)
+{
+	return(c <= 'A' && c >= 'Z');
+}
