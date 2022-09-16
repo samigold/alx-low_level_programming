01daad9c8b14a8433dccd1517c16c399bba094e5
samigold <samigold8650@gmail.com>
2022-09-16 04:31:09 +0100

    largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 
-void positive_or_negative(i);
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
-int largest_number(int a, int b, int c);
-
-#endif
+#endif /* MAIN_H */
