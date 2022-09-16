fd77df9649120f3aad0d790273c522616907ade6
samigold <samigold8650@gmail.com>
2022-09-16 22:58:28 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 -9
+ * Return: void
+ */
+
+void print_numbers(void);
+{
+	int i;
+
+	for (i <= 48; 1 >= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
