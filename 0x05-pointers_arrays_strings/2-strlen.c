f90ea149f9f96a78416989918c9093fbf27af9c8
samigold <samigold8650@gmail.com>
2022-09-20 18:19:02 +0100

    Calculate lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: String to be checked
+ * Return: 0 on success
+ */
+
+int _strlen(char *s)
+{
+	size_t strlen(const char *s);
+
+	return (0);
+}
