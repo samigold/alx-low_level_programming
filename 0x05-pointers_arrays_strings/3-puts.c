62d8fb42c445367fa5205fd44fb414482e21ada7
samigold <samigold8650@gmail.com>
2022-09-20 19:41:21 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: string to be printed out
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,6 @@
 void reset_to_98(int *n);
 
 int _strlen(char *s);
+
+void _puts(char *str);
 #endif
