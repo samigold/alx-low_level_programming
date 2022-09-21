b47c8b558eb1ecf01891d814b0a705d16851ce22
samigold <samigold8650@gmail.com>
2022-09-21 03:41:22 +0100

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: input string to reverse
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	for (*s[i] != '\0'; i++)
+	{
+		i -=1;
+		
+		while (i < j)
+		{
+			temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+
+			j++;
+		}
+		i--;
+	}
