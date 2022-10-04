4ecc939cf8f8588c898b3a7e63dd581eb882d94c
samigold <51761415+samigold@users.noreply.github.com>
2022-10-04 23:57:40 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,8 @@
  * @s2: This is the input string
  *
  * Return: The returned pointer should point to a newly allocated space in
- *         memory which contains the contents of s1, followed by the contents 
- *         of s2, and null terminated
+ * memory which contains the contents of s1, followed by the contents
+ * of s2, and null terminated
  */
 
 char *str_concat(char *s1, char *s2)
